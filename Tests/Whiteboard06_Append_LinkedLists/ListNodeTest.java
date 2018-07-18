936ff7eb134dff930e05e9bad6916995a932568b
Travis <travisjw@gmail.com>
2018-07-18 14:11:38 -0700

    kth node

---
+++ Tests/Whiteboard06_Append_LinkedLists/ListNodeTest.java
@@ -0,0 +1,41 @@
+package Whiteboard06_Append_LinkedLists;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListNodeTest {
+    ListNode TestList = new ListNode(5);
+
+    @BeforeEach
+    void setup(){
+        TestList.Append(7);
+        TestList.Append(4);
+        TestList.Append(156);
+        TestList.Append(42);
+    }
+
+
+    @Test
+    void RedTest (){
+        assertTrue(false);
+    }
+
+    @Test
+    void GreenTest(){
+        assertTrue(true);
+    }
+
+    @Test
+    void nthFromEndTest1() {
+        assertEquals(4, TestList.NthFromEnd(2).data);
+    }
+
+
+    @Test
+    void nthFromEndGrabEnd(){
+        assertEquals(42, TestList.NthFromEnd(0).data);
+    }
+}
--- src/Whiteboard06_Append_LinkedLists/ListNode.java
+++ src/Whiteboard06_Append_LinkedLists/ListNode.java
@@ -1,89 +1,93 @@
 package Whiteboard06_Append_LinkedLists;
 
-class ListNode
-{
-    public static void main(String[] args) {
 
+import java.util.List;
 
-        //Testing {
-            ListNode TestList = new ListNode(22, null);
-            TestList.Append(7);
-            TestList.Append(153);
+public class ListNode {
+    public static void main(String[]args){
 
-            TestList.InsertBefore(153, 14);
-            TestList.InsertBefore(14, 65);
-
-            TestList.InsertAfter(22, 3);
-
-            TestList.DeleteNode(22);
-            TestList.DeleteNode(153);
-
-            //Print List
-            ListNode current = TestList.root;
-            while (current.link != null){
-                System.out.print(current.data + "---->");
-                current=current.link;
-            }
-            System.out.println(current.data);
-
-        // }   End Testing   Expected result: 3, 7, 65, 14
     }
+
+    ///  ==================    Begin WB07
+
+    public ListNode NthFromEnd(int n) {
+        ListNode current = this.root;
+        ListNode next = this.root;
+        for (int i = 0; i != n; i++) {
+            next = next.link;
+        }
+
+        while (next.link != null) {
+            next = next.link;
+            current = current.link;
+        }
+
+        return current;
+    }
+    ///  ===================  End WB07
+
 
     protected int data;
     protected ListNode link;
-    protected ListNode root=this;
+    protected ListNode root = this;
+
 
     //Constructor to Build nodes
-    public ListNode(int d, ListNode n)
-    {
+    public ListNode(int d, ListNode n) {
         data = d;
         link = n;
     }
 
     //Constructor I'm actually using.
-    public ListNode (int d){
+    public ListNode(int d) {
         data = d;
         link = null;
     }
 
 
-    public void Append(int value){
+    public void Append(int value) {
         ListNode appendedNode = new ListNode(value);  ///*
         ListNode current = this.root;
-        while (current.link !=null){
+        while (current.link != null) {
             current = current.link;
         }
         current.link = appendedNode;
     }
 
-    public void InsertBefore(int TargetValue, int InsertedValue){
+    public void InsertBefore(int TargetValue, int InsertedValue) {
         //  If target value isn't in the list, automatically append to the end
-        ListNode appendedNode = new ListNode(InsertedValue); ///*
+        ListNode appendedNode = new ListNode(InsertedValue);
+        if (this.root.data == TargetValue) {
+            Append(InsertedValue);
+        }
+
+
         ListNode current = this.root;
-        while ( current.link != null && current.link.data != TargetValue){
+        while (current.link != null && current.link.data != TargetValue) {
             current = current.link;
         }
         appendedNode.link = current.link;
         current.link = appendedNode;
     }
 
-    public void InsertAfter (int TargetValue, int InsertedValue){
+    public void InsertAfter(int TargetValue, int InsertedValue) {
         //  If target value isn't in the list, automatically append to the end
         ListNode appendedNode = new ListNode(InsertedValue);   //*
         ListNode current = this.root;
-        while (current.link!=null && current.data != TargetValue){
+        while (current.link != null && current.data != TargetValue) {
             current = current.link;
         }
         appendedNode.link = current.link;
+
         current.link = appendedNode;
     }
 
-    public void DeleteNode (int TargetValue){
+    public void DeleteNode(int TargetValue) {
         ListNode current = this.root;
-        if (current.data==TargetValue) {
-            this.root=current.link;}
-        else{
-            while (current.link != null && current.link.data != TargetValue){
+        if (current.data == TargetValue) {
+            this.root = current.link;
+        } else {
+            while (current.link != null && current.link.data != TargetValue) {
                 current = current.link;
             }
             current.link = current.link.link;
