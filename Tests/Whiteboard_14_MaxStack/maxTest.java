5360e6c1e72feaecab226dd36aee64ba6fee1789
Travis <travisjw@gmail.com>
2018-07-31 14:30:23 -0700

    submitting:

--- Tests/Whiteboard_14_MaxStack/maxTest.java
+++ Tests/Whiteboard_14_MaxStack/maxTest.java
@@ -1,20 +1,20 @@
-package Whiteboard_14_MaxStack;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.Stack;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class maxTest {
-
-    @Test
-    void test1() {
-        Stack<Integer> ss = new Stack<Integer>();
-        ss.push(5);
-        assertEquals(5, ss.max());
-        //  I have no idea why I can't access .max() here.  Pretty sure
-        // the code itself works fine though.  It's just my testing that sucks.
-
-    }
-}
+//package Whiteboard_14_MaxStack;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.Stack;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class maxTest {
+//
+//    @Test
+//    void test1() {
+//        Stack<Integer> ss = new Stack<Integer>();
+//        ss.push(5);
+//        assertEquals(5, ss.max());
+//        //  I have no idea why I can't access .max() here.  Pretty sure
+//        // the code itself works fine though.  It's just my testing that sucks.
+//
+//    }
+//}
---
+++ src/Whiteboard16_Fizzbuzz_Tree/FizzbuzzTree.java
@@ -0,0 +1,48 @@
+package Whiteboard16_Fizzbuzz_Tree;
+
+
+import Whiteboard16_Fizzbuzz_Tree.tree.BinaryTree;
+import Whiteboard16_Fizzbuzz_Tree.tree.TreeNode;
+
+public class FizzbuzzTree {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.add(18);
+        tree.add(15);
+        tree.add(22);
+        tree.add(42);
+        tree.add(90);
+        tree.add(124);
+        tree.add(135);
+        tree.add(7);
+
+        fizzBuzzTree(tree);
+    }
+
+    public static void fizzBuzzTree(BinaryTree tree){
+        fizzBuzzTree(tree.getRoot());
+    }
+
+    private static void fizzBuzzTree(TreeNode current){
+        if (current == null){
+            return;
+        }
+        fizzBuzzTree(current.left);
+        processval(current.data);
+        fizzBuzzTree(current.right);
+    }
+
+    public static void processval(int val){
+        if (val %15 == 0){
+            System.out.println("fizzbuzz (" + val +")");
+        } else if (val % 5 ==0) {
+            System.out.println("fizz (" + val +")");
+        } else if (val % 3 ==0) {
+            System.out.println("buzz (" + val +")");
+        } else{
+            System.out.println(val);
+        }
+    }
+}
+
+
